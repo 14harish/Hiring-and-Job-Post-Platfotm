78a8d6bb8bdac92a7fddc4bb1c65ec7a1b94d797
14harish <harishpc2003@gmail.com>
2024-06-15 11:42:59 +0530

    new

---
+++ server/src/test/java/com/joblist/server/ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joblist.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
